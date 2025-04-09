7954cbbd5cb8ccb04901863d20079a3fe589d34c
danial ghofrani <danialghofrani1@gmail.com>
2025-04-09 15:11:13 +0330

    new updates on session 4

--- session_4/ml_s4_.py
+++ session_4/ml_s4_.py
@@ -2,17 +2,42 @@ from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 import time
 
 
-X,y = make_blobs(n_samples=100, n_features=2, centers=2, cluster_std=1.5)
+X,y = make_blobs(n_samples=10000, n_features=3, centers=4, cluster_std=6)
 
+svm_report = []
+print("SVM")
 start_time = time.time()
 svm_clf = SVC()
 svm_clf.fit(X,y)
 end_time = time.time()
 train_time = end_time - start_time
-reg_clf = LogisticRegression()
+svm_report.append(train_time)
 
-svm_clf.fit(X,y)
-clf.fit(X,y)
+print("Train Time : ",train_time)
+start_time = time.time()
+pred = svm_clf.predict(X)
+end_time = time.time()
+predict_time = end_time - start_time
+print("Predict Time : ",predict_time)
+print(classification_report(y, pred))
+
+print("-------------------------------------------------------")
+
+
+print("Logistic regression")
+start_time = time.time()
+reg_clf = LogisticRegression()
+reg_clf.fit(X,y)
+end_time = time.time()
+train_time = end_time - start_time
+print("Train Time : ",train_time)
+start_time = time.time()
+pred = reg_clf.predict(X)
+end_time = time.time()
+predict_time = end_time - start_time
+print("Predict Time : ",predict_time)
+print(classification_report(y, pred))
